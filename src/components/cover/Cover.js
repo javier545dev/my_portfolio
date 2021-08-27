c9229416e716f9d05f6543b1331197ae4c5becf0
Javier Fuentes <javierfuentes545@gmail.com>
2021-08-27 00:43:50 -0500

    responsive cover and navbar

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -26,6 +26,27 @@ const Cover = () => {
           ]}
         />
       </p>
+      <div className="footer-sns">
+        <div className="sns-links">
+          <a
+            href="https://www.linkedin.com/in/javier545dev/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin linkedin"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/javier545"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-facebook facebook"></i>
+          </a>
+          <a href="https://twitter.com/Jav545" target="_blank" rel="noreferrer">
+            <i className="fab fa-twitter twitter"></i>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,28 +13,6 @@ const Footer = () => {
         <h3 className="contact">Contact Me</h3>
         <p>And let get dowm to work</p>
       </div>
-      <div className="footer-sns">
-        <div className="design-by">My Social Networks</div>
-        <div className="sns-links">
-          <a
-            href="https://www.linkedin.com/in/javier545dev/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-linkedin linkedin"></i>
-          </a>
-          <a
-            href="https://www.facebook.com/javier545"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-facebook facebook"></i>
-          </a>
-          <a href="https://twitter.com/Jav545" target="_blank" rel="noreferrer">
-            <i className="fab fa-twitter twitter"></i>
-          </a>
-        </div>
-      </div>
       <div className="footer-sns1">
         <div className="design-by">Design for me XD</div>
         <div className="sns-links">
