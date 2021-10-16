97bd2529d6169785d5060afa40c7fc11dc8570bd
Javier Fuentes <javierfuentes545@gmail.com>
2021-10-16 00:37:40 -0500

    adjust css

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import './Cover.css'
-import coverVideo from '../../media/coverVideo.mp4'
+import coverVideo from '../../media/coverVideo.gif'
 import Typewriter from 'typewriter-effect'
 
 const Cover = () => {
   return (
     <div className="cover-container">
-      <video className="video" src={coverVideo} autoPlay loop muted />
-      <div>
-        <div className="glitch" data-text="Javier/Fuentes">
-          Javier/Fuentes
+      <img className="video" alt="video cover" src={coverVideo} />
+      <div className="title">
+        <div className="glitch" data-text="Javier">
+          Javier
         </div>
-        <div className="glow">Javier/Fuentes</div>
+        <div className="glow">Javier</div>
       </div>
 
+      <div>
+        <div className="glitch" data-text="Fuentes">
+          Fuentes
+        </div>
+        <div className="glow">Fuentes</div>
+      </div>
       <p>
         I am a
         <Typewriter
           options={{
-            strings: ['Developer', 'Designer', 'Creative'],
+            strings: ['Developer', 'Designer', 'Musician'],
             autoStart: true,
             loop: true,
             delay: 75,
@@ -33,17 +39,17 @@ const Cover = () => {
             target="_blank"
             rel="noreferrer"
           >
-            <i className="fab fa-linkedin linkedin"></i>
+            <i className="fab fa-linkedin linkedin1"></i>
           </a>
           <a
             href="https://www.facebook.com/javier545"
             target="_blank"
             rel="noreferrer"
           >
-            <i className="fab fa-facebook facebook"></i>
+            <i className="fab fa-facebook facebook1"></i>
           </a>
           <a href="https://twitter.com/Jav545" target="_blank" rel="noreferrer">
-            <i className="fab fa-twitter twitter"></i>
+            <i className="fab fa-twitter twitter1"></i>
           </a>
         </div>
       </div>
